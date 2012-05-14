7aa0c24fa36e8909761fbcf3f66a7c899670b29c
leandro silva <leandrosousa2001@gmail.com>
2012-05-13 23:23:32 -0300

    real firt comiit

---
+++ test/Timer3Activity.java
@@ -0,0 +1,13 @@
+package timer3.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Timer3Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
